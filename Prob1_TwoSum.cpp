3cf076b9003d86ebd4b4d835ab2c1fef4257d662
KTS-o7 <teju2403@gmail.com>
2023-08-21 21:13:22 +0530

    Added Comments to Problem 1

--- Prob1_TwoSum.cpp
+++ Prob1_TwoSum.cpp
@@ -1,4 +1,10 @@
-/*write the documentation for the following code*/
+/*
+1. We will take a map to store the element and its index.
+2. We will run a loop from i=0 to n, where n is the size of the array.
+3. We will check if the difference of the target and the current element is in the map or not.
+4. If it is not present, then we will insert the current element and its index in the map.
+5. If it is present, then we will return an array of current index and the index of the difference of the target and the current element. \
+*/
 // Time Complexity: O(N)
 // Space Complexity: O(N)
 
