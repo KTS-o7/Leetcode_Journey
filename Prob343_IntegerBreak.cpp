a8c77111bc5ce3df71d2e7eceba1f385e2f59cf2
Krishnatejaswi S <shentharkrishnatejaswi@gmail.com>
2023-10-06 09:52:03 +0530

    Added solution to Problem 343

--- Prob343_IntegerBreak.cpp
+++ Prob343_IntegerBreak.cpp
@@ -21,3 +21,27 @@ public:
         
     }
 };
+
+
+// DP Solution
+
+static const int fast_io = [](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp = {1, 2, 4, 6, 9, 12, 18, 27, 36};
+        int product = 1;
+        while(n>10){
+            product *= 3;
+            n -= 3;
+        }
+        product *= dp[n-2];
+        return product;
+    }
+};
