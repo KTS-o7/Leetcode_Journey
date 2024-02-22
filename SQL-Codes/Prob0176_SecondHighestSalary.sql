c57abbd0f4a5ffad8d7d62f18ca1ad0b7d6bfa56
Krishnatejaswi S <shentharkrishnatejaswi@gmail.com>
2024-02-22 19:02:18 +0530

    Added Solution to Problem 176

--- SQL-Codes/Prob0176_SecondHighestSalary.sql
+++ SQL-Codes/Prob0176_SecondHighestSalary.sql
@@ -5,6 +5,9 @@
 -- insert into Employee (id, salary) values ('2', '200')
 -- insert into Employee (id, salary) values ('3', '300')
 
+
+-- Solution 1
+
 SELECT
     IFNULL(
       (SELECT DISTINCT Salary
@@ -12,3 +15,7 @@ SELECT
        ORDER BY Salary DESC
         LIMIT 1 OFFSET 1),
     NULL) AS SecondHighestSalary
+
+-- Solution 2
+SELECT MAX(salary) as SecondHighestSalary FROM Employee
+WHERE salary<(SELECT MAX(salary) FROM Employee) LIMIT 1;   
