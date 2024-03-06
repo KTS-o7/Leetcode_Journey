02d4c847d02436438edb595b89657f1f8e5b3ce2
Krishnatejaswi S <shentharkrishnatejaswi@gmail.com>
2024-03-06 10:59:37 +0530

    Modified Solution to Problem 141

--- Prob0141_LinkedListCycle.cpp
+++ Prob0141_LinkedListCycle.cpp
@@ -31,22 +31,41 @@ public:
 // Link to check : https://www.geeksforgeeks.org/floyds-cycle-finding-algorithm/
 // Optimum Solution
 
-class Solution
-{
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
 public:
-    bool hasCycle(ListNode *head)
-    {
-        ListNode *slow = head;
-        ListNode *fast = head;
-
-        while (fast && fast->next)
+    bool hasCycle(ListNode *head) {
+       // std::ios::sync_with_stdio(false);
+        if(head == NULL)
+            return false;
+        ListNode* first = head;
+        ListNode* sec = head;
+        while(first != NULL && first->next != NULL)
         {
-            slow = slow->next;
-            fast = fast->next->next;
-            if (slow == fast)
+            first=first->next->next;
+            sec=sec->next;
+
+            if(first == sec)
                 return true;
         }
-
         return false;
     }
 };
+
+// Time complexity : O(n)
+// Space complexity : O(1)
+// Code explained :
+// 1. We have two pointers
+// 2. First pointer moves 2 steps at a time
+// 3. Second pointer moves 1 step at a time
+// 4. If there is a cycle, then both pointers will meet at some point
+// 5. If there is no cycle, then first pointer will reach the end of the list
+// 6. So, we can return false in that case
+// 7. If there is a cycle, then we can return true
