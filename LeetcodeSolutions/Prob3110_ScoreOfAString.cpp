9ec0d107079750820f464daf306dae7a4355ff67
Krishnatejaswi S <shentharkrishnatejaswi@gmail.com>
2024-04-14 00:10:20 +0530

    Added solution to Problem 3110

--- LeetcodeSolutions/Prob3110_ScoreOfAString.cpp
+++ LeetcodeSolutions/Prob3110_ScoreOfAString.cpp
@@ -5,7 +5,7 @@ public:
         int score = 0;
         for(int i = 0;i<s.size()-1;i++)
         {
-            score+= abs((int)(s[i]-s[i+1]));
+            score+= abs(s[i]-s[i+1]);
         }
         return score;
     }
