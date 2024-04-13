9b27061921efdb497c5ffeb765dae9f7278c1e24
Krishnatejaswi S <shentharkrishnatejaswi@gmail.com>
2024-04-14 00:09:56 +0530

    Added solution to Problem 3100

---
+++ LeetcodeSolutions/Prob3110_ScoreOfAString.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        std::ios::sync_with_stdio(false);
+        int score = 0;
+        for(int i = 0;i<s.size()-1;i++)
+        {
+            score+= abs((int)(s[i]-s[i+1]));
+        }
+        return score;
+    }
+};
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+// Logic explained :
+// 1. We will iterate over the string and calculate the absolute difference between the ascii values of the current character and the next character.
+// 2. We will add the absolute difference to the score.
+// 3. Finally we will return the score.
